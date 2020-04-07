6ef1b8ba2a1d8317c71d85d4e177dba84a827f5b
LPugens <lpugensf@gmail.com>
2020-04-07 20:57:14 -0300

    update

--- remote_run.py
+++ remote_run.py
@@ -13,7 +13,7 @@ zone = 'asia-east1-a'
 machine_type = 'n1-standard-1'
 bucket = 'datasets_pugens'
 repository = 'https://github.com/LPugens/pytorch_experiments'
-use_gpu = False
+use_gpu = True
 
 
 compute = googleapiclient.discovery.build('compute', 'v1')
@@ -25,13 +25,13 @@ while instance_name in instances:
 vm = VirtualMachine(name=instance_name, project=project, zone=zone, machine_type=machine_type, use_gpu=use_gpu)
 try:
     vm.instantiate( bucket, repository)
-    # vm.send_files('./cloud/startup_environment.sh')
-    # vm.send_files('./cloud/startup_conda.sh')
-    # vm.send_files('./cloud/run_script.sh')
-    # vm.run_command('sudo ./startup_environment.sh')
-    # vm.reboot()
-    # vm.run_command('./startup_conda.sh')
-    # vm.run_command('./run_script.sh')
+    vm.send_files('./cloud/startup_environment.sh')
+    vm.send_files('./cloud/startup_conda.sh')
+    vm.send_files('./cloud/run_script.sh')
+    vm.run_command('sudo ./startup_environment.sh')
+    vm.reboot()
+    vm.run_command('./startup_conda.sh')
+    vm.run_command('./run_script.sh')
     input('Press ENTER to finish the VM')
 except Exception as e:
     vm.logger.stop_log()
